d84fa274c5a316c9bea002d4b64353b1a42cfd31
MCX <mc@mcx.ink>
2022-07-09 21:55:14 -0600

    Static Files - Success!

--- web/mazorka/cdn/conf.py
+++ web/mazorka/cdn/conf.py
@@ -1,7 +1,7 @@
 import os
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "django-cr4e"
 AWS_S3_ENDPOINT_URL = "https://nyc3.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETER = {
