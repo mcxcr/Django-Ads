c5fbd7d676e9b6647376cd414e0f4aba42499e1e
MCX <mc@mcx.ink>
2022-07-09 21:22:52 -0600

    CSRF Trusted Origins corrected - attempt 4.

--- web/mazorka/settings.py
+++ web/mazorka/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 if ENV_ALLOWED_HOST:
     ALLOWED_HOSTS = [ENV_ALLOWED_HOST]
 
-CSRF_TRUSTED_ORIGINS = ['https://stage.cr4ever.com', 'https://cr4ever.com']
+# CSRF_TRUSTED_ORIGINS = ['https://stage.cr4ever.com', 'https://cr4ever.com']
+CSRF_TRUSTED_ORIGINS = ['stage.cr4ever.com', 'cr4ever.com']
 
 # Application definition
 
